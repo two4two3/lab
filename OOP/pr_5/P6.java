052d422f1d4d7b8b0535b9cd166769e72c76ddb7
miteshchopda <miteshchopda00@gmail.com>
2026-08-23 19:42:22 +0530

    java lab 5 practical 6

--- OOP/pr_5/P6.java
+++ OOP/pr_5/P6.java
@@ -6,27 +6,29 @@
 // *       *
 // * * * * *
 
-public class __P6 {
+public class P6 {
     void printPyramid(int n)
     {
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(i!=0 && j!=0 ||  i!=n-1 && j!=0
-                    || i!=0 && j!=n-1 ||  i!=n-1 && j!=n-1
-                    || i==0 && j!=0 || i==0 && j!=n-1
-                    || i==n-1 && j!=0 || i==n-1 && j!=n-1
-                ){
+        for(int i=0; i<n; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+        for(int i=1; i<n-1; i++){
+            for(int j=0; j<n; j++){
+                if(j==0 || j==n-1){
                     System.out.print("*");
                 }else{
-                    System.out.println(" ");
+                    System.out.print(" ");
                 }
             }
             System.out.println();
         }
+        for(int i=0; i<n; i++){
+            System.out.print("*");
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
-        new __P6().printPyramid(5);
+        new P6().printPyramid(5);
     }
 }
