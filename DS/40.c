d633f6009ec58e05e3d6716c2e439d7fcca2884a
miteshchopda <miteshchopda00@gmail.com>
2026-08-30 15:25:02 +0530

    DS LAB 9 UPDATE

--- DS/40.c
+++ DS/40.c
@@ -1,14 +1,15 @@
+// 40. Write a recursive function to convert a decimal number into its binary
+// equivalent.
 #include "stdio.h"
 
-int d_to_b(int decimal, int bin) {
+int d_to_b(int decimal) {
   if (decimal == 0)
-    return bin;
-  bin = bin * 10;
-  return d_to_b(decimal / 2, bin + decimal % 2);
+    return 0;
+  return decimal % 2 + 10 * d_to_b(decimal / 2);
 }
 void main() {
   int decimal;
   printf("Enter a number to convert to binary:");
   scanf("%d", &decimal);
-  printf("%d\n", d_to_b(decimal, 0));
+  printf("%d\n", d_to_b(decimal));
 }
